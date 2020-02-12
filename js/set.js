f5c41dddc3433ebb6d6491403679518a2c96f654
userzhang <1482901238@qq.com>
2020-02-12 14:47:01 +0800

    1、删除了多余的数据 2、新增了选手数据雷达图

--- js/set.js
+++ js/set.js
@@ -1,7 +1,26 @@
+setWindow();
+setChoose(["1", "2", "3"]);
+//选择比赛
+$(".copm").click(function(d){
+	console.log(d.currentTarget.id)
+})
+/**
+ * 设置窗口大小
+ */
 function setWindow() {
 	let height = window.innerHeight || document.body.clientHeight;
 	let width = window.innerWidth || document.documentElement.clientWidth;
 	document.getElementById("mainShow").style.height = height+'px';
-	document.getElementById("mainShow").style.width = width+'px';
+	document.getElementById("mainShow").style.width = width*0.98+'px';
 }
-setWindow();
+/**设置选择框数据
+ * @param {Object} data
+ */
+function setChoose(data) {
+	let copData = data;
+	for (let d in copData){
+		let html = "<li class="+"copm"+" id=copm-"+d+"><a>"+d+"</a></li>";
+		$("#copMenu").append(html);
+	}
+}
+
