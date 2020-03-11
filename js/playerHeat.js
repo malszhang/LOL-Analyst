5d6cb89ae72378bb29bfe551c20fd3238247d660
zhouyang <320235617@qq.com>
2020-03-11 21:49:57 +0800

    调整坐标轴位置。

--- js/playerHeat.js
+++ js/playerHeat.js
@@ -4,74 +4,81 @@ var heatChar = echarts.init(playerHeat);
 $.get("data/player/playerHeat.json", function(data) {
 	drawPlayerHeat(data);
 });
-function dealHeatData(data){
+
+function dealHeatData(data) {
 	let dealData = {};
 	let xData = data.data.map(function(d) {
 		return parseFloat(d.axis_x);
 	});
-	xData.sort(function(a,b){
-		return a-b;
+	xData.sort(function(a, b) {
+		return a - b;
 	});
 	let yData = data.data.map(function(d) {
 		return parseFloat(d.axis_y);
 	});
-	yData.sort(function(a,b){
-		return a-b;
+	yData.sort(function(a, b) {
+		return a - b;
 	});
 	let heatNum = data.data.map(function(d) {
 		return parseFloat(d.hot_value);
 	});
 	// let hotMax = Math.max.apply(Math,heatNum);
-	
+
 	let showData = data.data.map(function(d) {
-		return [parseFloat(d.axis_x), parseFloat(d.axis_y),parseFloat(d.hot_value)]
+		return [parseFloat(d.axis_x), parseFloat(d.axis_y), parseFloat(d.hot_value)]
 	});
 	dealData.xData = xData;
 	dealData.yData = yData;
 	dealData.showData = showData;
 	dealData.heatNum = heatNum;
-	return dealData; 
+	return dealData;
 }
 
-function drawPlayerHeat(data){
+function drawPlayerHeat(data) {
 	let dealData = dealHeatData(data);
-	let minNum = Math.min.apply(Math,dealData.heatNum);
-	let maxNum = Math.max.apply(Math,dealData.heatNum);
+	let minNum = Math.min.apply(Math, dealData.heatNum);
+	let maxNum = Math.max.apply(Math, dealData.heatNum);
 	let option = {
-	    tooltip: {},
-	    xAxis: {
-	        type: 'value',
-	        data: dealData.xData,
+		tooltip: {},
+		grid: {
+			x: 0,
+			y: 0,
+			x2: 0,
+			y2: 0,
+		},
+		xAxis: {
+			type: 'value',
+			data: dealData.xData,
 			show : false
-	    },
-	    yAxis: {
-	        type: 'value',
-	        data: dealData.yData,
-			show: false
-	    },
-	    visualMap: {
+		},
+		yAxis: {
+			type: 'value',
+			data: dealData.yData,
+			show: false,
+			inverse: true
+		},
+		visualMap: {
 			type: 'continuous',
-	        min: minNum,
-	        max: maxNum,
-	        calculable: true,
-	        realtime: false,
+			min: minNum,
+			max: maxNum,
+			calculable: true,
+			realtime: false,
 			dimension: 2,
 			inRange: {
-						color: 'red',
-						colorAlpha: [0.4, 1],
-			        } ,
+				color: 'red',
+				colorAlpha: [0.4, 1],
+			},
 			show: false
-	    },
-	    series: [{
-	        name: 'Gaussian',
-	        type: 'heatmap',
-	        data: dealData.showData,
-			emphasis:{
+		},
+		series: [{
+			name: 'Gaussian',
+			type: 'heatmap',
+			data: dealData.showData,
+			emphasis: {
 				shadowColor: 'yellow'
 			}
-	    }]
+		}]
 	};
-	
+
 	heatChar.setOption(option);
 }
-
