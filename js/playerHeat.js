fe091f85bc5f0d412a016a1abce4ba278192ac5a
userzhang <1482901238@qq.com>
2020-03-20 14:36:34 +0800

    新增轨迹图处理

--- js/playerHeat.js
+++ js/playerHeat.js
@@ -1,6 +1,8 @@
 var playerHeat = document.getElementById("playerHeat")
 var heatChar = echarts.init(playerHeat);
-
+var heatHeight = $("#playerHeat").height();
+var heatWidth = $("#playerHeat").width();
+var squareLen = heatHeight > heatWidth? heatWidth: heatHeight;
 $.get("data/player/playerHeat.json", function(data) {
 	drawPlayerHeat(data);
 });
@@ -41,15 +43,15 @@ function drawPlayerHeat(data) {
 	let option = {
 		tooltip: {},
 		grid: {
-			x: 0,
-			y: 0,
-			x2: 0,
-			y2: 0,
+			bottom: 0,
+			top: 0,
+			right: 0,
+			left: heatWidth-squareLen
 		},
 		xAxis: {
 			type: 'value',
 			data: dealData.xData,
-			show : false
+			show: false
 		},
 		yAxis: {
 			type: 'value',
@@ -65,10 +67,26 @@ function drawPlayerHeat(data) {
 			realtime: false,
 			dimension: 2,
 			inRange: {
-				color: 'red',
-				colorAlpha: [0.4, 1],
+				color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43',
+					'#d73027', '#a50026'
+				]
 			},
 			show: false
+		},
+		graphic: {
+			type: 'image',
+			id: 'logo',
+			bottom: 0,
+			right: 0,
+			z: -10,
+			// bounding: 'raw',
+			// origin: [75, 75],
+			style: {
+				image: 'img/map.png',
+				width: squareLen,
+				height: squareLen,
+				opacity: 0.9
+			}
 		},
 		series: [{
 			name: 'Gaussian',
--- js/trace.js
+++ js/trace.js
@@ -1,61 +1,111 @@
 var traceDom = document.getElementById("traceFlow");
 var traceChart = echarts.init(traceDom);
-traceDraw();
-function traceDraw(){
+var colors = ["#E9B9A7", "#E99E9A", "#A9272C", "#DCB1B8", "#E3A3C0",
+	"#AEADCD", "#A5C0D0", "#ADCAC8", "#B5D4C2", "#71B3B8"
+];
+$.get("json/Edg-vs-Blg.json", function(data) {
+	traceDraw(data);
+});
+
+function traceDraw(data) {
 	option = {
-	    animation: false,
-	    grid: {
-	        top: 40,
-	        left: 50,
-	        right: 40,
-	        bottom: 50
-	    },
-	    xAxis: {
-	        name: 'x',
-	        minorTick: {
-	            show: true
-	        },
-	        splitLine: {
-	            lineStyle: {
-	                color: '#999'
-	            }
-	        },
-	        minorSplitLine: {
-	            show: true,
-	            lineStyle: {
-	                color: '#ddd'
-	            }
-	        }
-	    },
-	    yAxis: {
-	        name: 'y',
-	        min: -100,
-	        max: 100,
-	        minorTick: {
-	            show: true
-	        },
-	        splitLine: {
-	            lineStyle: {
-	                color: '#999'
-	            }
-	        },
-	        minorSplitLine: {
-	            show: true,
-	            lineStyle: {
-	                color: '#ddd'
-	            }
-	        }
-	    },
-	    series: [
-	        {
-	            type: 'line',
-	            showSymbol: false,
-	            clip: true,
-	            data: [[1,2], [2,3], [4,5], [6,7]]
-	        }
-	    ]
+		animation: false,
+		grid: {
+			top: 40,
+			left: 5,
+			right: 5,
+			bottom: 50
+		},
+		legend: {
+			data: data[1],
+			left: 10
+		},
+		dataZoom: [{
+				show: true,
+				realtime: true,
+				start: 65,
+				end: 85,
+				xAxisIndex: [0, 1]
+			},
+			{
+				type: 'inside',
+				realtime: true,
+				start: 65,
+				end: 85,
+				xAxisIndex: [0, 1]
+			}
+		],
+		grid: [{
+			left: 5,
+			right: 0,
+			height: '40%'
+		}, {
+			left: 5,
+			right: 0,
+			top: '64%',
+			height: '20%',
+		}],
+		visualMap: {
+			show: false,
+			top: 10,
+			right: 10,
+			seriesIndex: 0,
+			pieces: [{
+				gt: 0,
+				lte: 30000,
+				color: 'red'
+			}, {
+				gt: -30000,
+				lte: 0,
+				color: 'blue'
+			}]
+		},
+		xAxis: [{
+			name: 'x',
+			type: 'category',
+		}, {
+			name: 'x1',
+			gridIndex: 1,
+			type: 'category',
+			position: 'top',
+			show: false
+		}],
+		yAxis: [{
+			name: 'y',
+			show: false,
+		}, {
+			name: 'economy',
+			// min:Math.min.apply(Math, data[2]),
+			// max:Math.max.apply(Math, data[2]),
+			gridIndex: 1,
+			show: false
+		}],
+		series: [{
+			xAxisIndex: 1,
+			yAxisIndex: 1,
+			type: 'line',
+			stack: '1',
+			smooth: true,
+			symbol: 'none',
+			areaStyle: {},
+			data: data[2]
+		}]
 	};
+
+	for (var i = 0; i < 10; i++) {
+		option.series.push({
+			name: data[1][i],
+			yAxisIndex: 0,
+			type: 'line',
+			showSymbol: false,
+			color: colors[i],
+			lineStyle: {
+				width: 1,
+			},
+			clip: true,
+			smooth: true,
+			data: data[0][i]
+		});
+	}
 	traceChart.setOption(option)
 }
-
-	
